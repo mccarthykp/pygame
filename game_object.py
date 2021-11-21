2401d270f16c13b575f596f25a501f586c44d729
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-11-21 16:57:01 -0500

    added bomb collision game reset

--- game_object.py
+++ game_object.py
@@ -6,6 +6,9 @@ class GameObject(pygame.sprite.Sprite):
     self.surf = pygame.image.load(image)
     self.x = x
     self.y = y
+    self.rect = self.surf.get_rect()
   
   def render(self, screen):
+    self.rect.x = self.x
+    self.rect.y = self.y
     screen.blit(self.surf, (self.x, self.y))
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import pygame
 from fruits import Apple, Strawberry
 from player import Player
 from bomb import Bomb
+from random import randint
 
 pygame.init()
 screen = pygame.display.set_mode([500, 500])
@@ -14,6 +15,7 @@ bomb = Bomb()
 
 running = True
 while running:
+  collected = 0
   screen.fill((255, 255, 255))
   
   # Looks at events
@@ -39,9 +41,31 @@ while running:
   all_sprites.add(strawberry)
   all_sprites.add(bomb)
 
+  fruit_sprites = pygame.sprite.Group()
+  fruit_sprites.add(apple)
+  fruit_sprites.add(strawberry)
+
   for entity in all_sprites:
     entity.move()
     entity.render(screen)
+
+    fruit = pygame.sprite.spritecollideany(player, fruit_sprites)
+    if fruit:
+      fruit.reset()
+      collected += 1
+      apple.dy += 0.05
+      strawberry.dx += 0.05
+
+    if pygame.sprite.collide_rect(player, bomb):
+      bomb.reset()
+      player.dx = 220
+      player.dy = 220
+      apple.reset()
+      apple.dy = (randint(0, 200) / 100) + 1
+      strawberry.reset()
+      strawberry.dx = (randint(0, 200) / 100) + 1
+      collected = 0
+      # running = False
 
   pygame.display.flip()
   clock.tick(90)
