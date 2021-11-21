dad72861766c78a86067ed98e19f5debbf58f6db
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-11-21 16:18:04 -0500

    bomb direction is functioning properly

--- bomb.py
+++ bomb.py
@@ -13,16 +13,37 @@ class Bomb(GameObject):
     self.x += self.dx
     self.y += self.dy
 
-    if self.x > 500 or self.x < -64 or self.y > 500 or self.y < -64:
-      self.reset()
-
-    if self.x > 500:
+    if self.x > 564 or self.x < -64 or self.y > 564 or self.y < -64:
       self.reset()
 
   # reset should randomly choose whether the bomb will move up, down, left or right
+  # def reset(self):
+  #   x_coordinates = [93, 218, 343]
+  #   y_coordinates = [-64, 564]
+  #   self.x = choice(x_coordinates)
+  #   self.y = choice(y_coordinates)
+
   def reset(self):
-    x_coordinates = [93, 218, 343]
-    y_coordinates = [-64, 564]
-    self.x = choice(x_coordinates)
-    self.y = choice(y_coordinates)
+    direction = randint(1, 4)
+    coordinates = [93, 218, 343]
+    if direction == 1: # left
+      self.x = 564
+      self.y = choice(coordinates)
+      self.dx = ((randint(25, 200) / 100) + 1) * - 1
+      self.dy = 0
+    elif direction == 2: # right
+      self.x = -64
+      self.y = choice(coordinates)
+      self.dx = (randint(25, 200) / 100) + 1
+      self.dy = 0
+    elif direction == 3: # down
+      self.x = choice(coordinates)
+      self.y = -64
+      self.dx = 0
+      self.dy = (randint(25, 200) / 100) + 1
+    else:
+      self.x = choice(coordinates)
+      self.y = 564
+      self.dx = 0
+      self.dy = ((randint(25, 200) / 100) + 1) * -1
     
